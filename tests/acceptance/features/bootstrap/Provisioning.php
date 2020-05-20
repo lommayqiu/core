6211f0232921992410516c7fc557cd842255647a
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-05-20 13:42:27 +0545

    webUIManageQuota

--- tests/acceptance/features/bootstrap/Provisioning.php
+++ tests/acceptance/features/bootstrap/Provisioning.php
@@ -1591,6 +1591,7 @@ trait Provisioning {
 	public function adminHasChangedTheQuotaOfUserTo(
 		$user, $quota
 	) {
+		$user = $this->getActualUsername($user);
 		$this->adminChangesTheQuotaOfUserUsingTheProvisioningApi(
 			$user, $quota
 		);
